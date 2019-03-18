50d32c78db65759d7126cd8a318440cbd6d487b0
Leonel Nieto <43765662+leonelnieto@users.noreply.github.com>
2019-03-18 07:00:28 -0600

    Updates

---
+++ precache-manifest.723ab22cbeb82707056c69de689a2b88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shsp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9724497c2f5138c10beb",
+    "url": "/shsp/static/js/runtime~main.4cef3658.js"
+  },
+  {
+    "revision": "0659f0cd48eefd80d494",
+    "url": "/shsp/static/js/main.9830c679.chunk.js"
+  },
+  {
+    "revision": "1d326632bbb0ea4bb63e",
+    "url": "/shsp/static/js/2.d2b4789b.chunk.js"
+  },
+  {
+    "revision": "0659f0cd48eefd80d494",
+    "url": "/shsp/static/css/main.1372ebb1.chunk.css"
+  },
+  {
+    "revision": "233432748201513ec48bead805f0c058",
+    "url": "/shsp/index.html"
+  }
+];
