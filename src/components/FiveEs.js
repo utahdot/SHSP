1b44f5ab9a0d620ae8d6fba30982174aeffe11a8
Philip Nacamuli <panac@nacamuli.net>
2019-04-24 21:30:31 -0500

    create cards for each category on five e's page

--- src/components/FiveEs.js
+++ src/components/FiveEs.js
@@ -1,12 +1,155 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 class FiveEs extends Component {
   render() {
     return (
       <div className="m-5">
         <div className="row">
-          <div className="col-12">
-            <h1 className="text-center">Five E's</h1>
+          <div className="col-12 mx-4">
+            <h3 className="text-center">Empahsis Safety Areas</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 mx-4 text-center">
+            <p>
+              Emphasis safety areas consist of 12 programs that will be given
+              added attention and emphasis by the safety organizations for the
+              next five years
+            </p>
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center text-center">
+          <div className="col-md-2 border-0">
+            <div className="card h-100">
+              <img
+                className="card-img-top"
+                src="img/aggresive-driving.png"
+                alt="Engineering"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Engineering</h5>
+                <p className="card-text">
+                  The focus on safety within engineering begins
+                  with designing and building safe roadways.
+                  Transportation engineers use design principles
+                  that have been proven to be safe and reliable.
+                  National standards are used for signs and traffic
+                  markings to provide consistency for the traveling
+                  public. However, engineers continue to look for
+                  new and innovative ways to make roads safer.
+                </p>
+              </div>
+            </div>
+            <NavLink className="mt-3 btn btn-outline-primary" to="/test">
+              Engineering
+            </NavLink>
+          </div>
+          <div className="col-md-2 border-0">
+            <div className="card h-100">
+              <img
+                className="card-img-top"
+                src="img/distracted-driving.png"
+                alt="Enforcement"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Enforcement</h5>
+                <p className="card-text">
+                  Unfortunately, despite the best safety education
+                  programs and safely designed and built
+                  roadways, enforcement is needed to remind
+                  people of the laws associated with the use of our
+                  roadway network. State, county, and municipal
+                  law enforcement agencies statewide work
+                  alongside highway safety partnering agencies to
+                  enforce Utah’s traffic laws during regular patrols,
+                  as well as specialized mobilization efforts.
+                </p>
+              </div>
+            </div>
+            <NavLink className="mt-3 btn btn-outline-primary" to="/test">
+              Enforcement
+            </NavLink>
+          </div>
+          <div className="col-md-2 border-0">
+            <div className="card h-100">
+              <img
+                className="card-img-top"
+                src="img/drowsy-driving.png"
+                alt="Education"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Education</h5>
+                <p className="card-text">
+                  Education plays a key role in helping the public
+                  understand what they should and should not
+                  do when driving. Increased education leads to
+                  a change in habits and, ultimately, a decline in
+                  fatalities on our roads. Education efforts are
+                  directed toward all age groups and numerous
+                  safety issues.
+                </p>
+              </div>
+            </div>
+            <NavLink className="mt-3 btn btn-outline-primary" to="/test">
+              Education
+            </NavLink>
+          </div>
+          <div className="col-md-2 border-0">
+            <div className="card h-100">
+              <img
+                className="card-img-top"
+                src="img/impaired-driving.png"
+                alt="Emergency Response"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Emergency Response</h5>
+                <p className="card-text">
+                  Swift response from emergency teams can
+                  save lives. Trained dispatchers ensure the right
+                  resources arrive to provide care and address
+                  safety at the scene. Emergency medical response
+                  teams provide life saving health care and ensure
+                  the patients are transported to the facility that
+                  best meets the needs of the patients. Public
+                  safety responders clear roadways, thereby
+                  reducing the risk of more crashes happening as
+                  a result of unusual traffic flow. Keeping roadways
+                  clear is vital to traffic safety and essential for
+                  preventing further problems due to unsafe road
+                  conditions.
+                </p>
+              </div>
+            </div>
+            <NavLink className="mt-3 btn btn-outline-primary" to="/test">
+              Emergency Response
+            </NavLink>
+          </div>
+          <div className="col-md-2 border-0">
+            <div className="card h-100">
+              <img
+                className="card-img-top"
+                src="img/aggresive-driving.png"
+                alt="Everyone"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Everyone</h5>
+                <p className="card-text">
+                  No matter how hard we try to engineer the roads
+                  so people can get to their destinations safely,
+                  no matter how hard we try to educate drivers
+                  about safe driving, no matter how hard we try to
+                  enforce the laws or respond in an emergency, the
+                  ultimate responsibility rests upon everyone who
+                  gets in a car. We all need to work together if we
+                  are going to reach Zero Fatalities. Everyone is the
+                  most important “E” in traffic safety.
+                </p>
+              </div>
+            </div>
+            <NavLink className="mt-3 btn btn-outline-primary" to="/test">
+              Everyone
+            </NavLink>
           </div>
         </div>
       </div>
